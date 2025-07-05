2d458d0048f4460a6aa0d891f87745a88c84056f
nickyshox99 <nickyshox99@gmail.com>
2025-07-06 00:04:53 +0700

    x

--- src/controllers/linewebhook.controller.js
+++ src/controllers/linewebhook.controller.js
@@ -592,7 +592,7 @@ module.exports = function(wsConnections) {
       console.log(error.message);
       res.status(202).json({
         status: "error",
-        message : error.message,
+        message : error,
       });
     }
   }
